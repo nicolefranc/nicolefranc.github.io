d2021a6c6fac039ff4e79e38ff47dc8ab1e08f74
Nicole <nicoleyu@outlook.sg>
2021-09-12 02:49:08 +0800

    Update landing responsiveness

--- src/sections/Landing.vue
+++ src/sections/Landing.vue
@@ -3,22 +3,27 @@
     <div class="relative mx-auto my-0">
       <!-- Text -->
       <div
-        class="absolute z-10 mt-12 xs:mt-20 md:mt-24 ml-12 xs:ml-20 md:ml-24 text-white"
+        class="absolute z-10 mt-14 xs:mt-24 ml-16 xs:ml-20 md:ml-24 text-white"
       >
-        <h1 class="font-mono xs:text-2xl">
+        <h1 class="font-mono font-semibold text-2xl">
           <span>Hi, my name is </span>
+          <span class="md:hidden"><br /></span>
           <span class="text-yellow-orange font-bold xs:text-4xl">Nicole.</span>
         </h1>
         <h2
-          class="font-mono font-light xs:font-normal my-2 xs:my-4 text-xs xs:text-base"
+          class="font-mono font-normal my-2 xs:my-4 text-xs xs:text-base leading-loose"
         >
-          I'm a Computer Science student<br />and aspiring Software Engineer.
+          I'm a
+          <span class="border-b-2 border-yellow-orange">Computer Science</span>
+          student<br />and aspiring
+          <span class="border-b-2 border-yellow-orange">Software Engineer</span
+          >.
         </h2>
-        <a
+        <!-- <a
           href="#projects"
           class="font-mono font-light text-xs xs:text-sm border-b-2 border-yellow-orange hover:bg-indigo hover:text-yellow-orange"
           >View Projects</a
-        >
+        > -->
       </div>
       <!-- Terminal -->
       <img
@@ -30,7 +35,7 @@
       <img
         src="../assets/images/avatar.png"
         alt="Avatar of Nicole"
-        class="w-1/3 relative mx-auto -mt-20 xs:-mt-40"
+        class="w-1/3 relative mx-auto -mt-20 xs:-mt-48"
       />
     </div>
   </header>
