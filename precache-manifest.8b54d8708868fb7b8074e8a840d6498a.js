6aa7e247a769c1293388a8e36bc04599ecf06d1e
Nicole <nicoleyu@outlook.sg>
2021-09-12 02:53:19 +0800

    gh-pages

---
+++ precache-manifest.8b54d8708868fb7b8074e8a840d6498a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6dd554b8307ebc324c",
+    "url": "/css/app.65429630.css"
+  },
+  {
+    "revision": "a2d44fb74ebab1855ad3541581ce4ca8",
+    "url": "/img/avatar.a2d44fb7.png"
+  },
+  {
+    "revision": "9dd4203f5af96a07b048f63b39e8fe35",
+    "url": "/img/foreign.9dd4203f.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/img/github.1f33e75d.svg"
+  },
+  {
+    "revision": "f0c895fbaac1ae29d7e553c06e188be5",
+    "url": "/img/overdue-home.f0c895fb.png"
+  },
+  {
+    "revision": "2bbbc8452f3026be57b06abbdcf4df79",
+    "url": "/img/terminal.2bbbc845.png"
+  },
+  {
+    "revision": "613facd3cf3229f783648b3a82936dc6",
+    "url": "/img/virtual-adss-home.613facd3.png"
+  },
+  {
+    "revision": "3dcbc196c5347f197774de9b1933d348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d2dab9bcbc45db1ce60",
+    "url": "/js/about.ecb28293.js"
+  },
+  {
+    "revision": "ff6dd554b8307ebc324c",
+    "url": "/js/app.c39d3a96.js"
+  },
+  {
+    "revision": "08cbe73bc9fd5fe27818",
+    "url": "/js/chunk-vendors.bc689da9.js"
+  },
+  {
+    "revision": "0b11628a4ebf92bd220e5534eb34c7ee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c70d14cd74462dd4f3f5af67f23c7d29",
+    "url": "/site.webmanifest"
+  }
+]);
